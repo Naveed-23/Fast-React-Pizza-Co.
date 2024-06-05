4d626fb853a6fea762e2d2f7424d15ff796a87e2
Naveed-23 <naveedbh23@gmail.com>
2024-06-06 00:33:45 +0530

    Intial commit

---
+++ src/features/cart/CartOverview.jsx
@@ -0,0 +1,23 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
+import { formatCurrency } from "../../utils/helpers";
+
+
+function CartOverview() {
+  const totalCartQuanitiy = useSelector(getTotalCartQuantity);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+
+  if(!totalCartQuanitiy) return null;
+  return (
+    <div className="flex items-center justify-between bg-stone-800 text-sm text-stone-200 uppercase px-4 py-4 sm:px-6 md:text-base">
+      <p className="text-stone-300 font-semibold space-x-4 sm:space-x-6">
+        <span>{totalCartQuanitiy} pizzas</span>
+        <span>{formatCurrency(totalCartPrice)}</span>
+      </p>
+      <Link to='/cart'>Open cart &rarr;</Link>
+    </div>
+  );
+}
+
+export default CartOverview;
---
+++ src/features/user/Username.jsx
@@ -0,0 +1,9 @@
+import { useSelector } from "react-redux"
+
+
+export default function Username() {
+    const username = useSelector(store=> store.user.username)
+
+    if(!username) return null;
+    return <div className="text-sm font-semibold hidden md:block">{username}</div>
+}
